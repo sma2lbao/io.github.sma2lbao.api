59501f6c524b470ad127c2937389453e71b371dc
李观保 <sma2lbao@qq.com>
2020-06-04 18:23:54 +0800

    users and auth module initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { schemas, configs } from './config'
+import { AuthModule } from './shared/auth/auth.module';
+import { UsersModule } from './shared/users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -10,7 +12,7 @@ import { schemas, configs } from './config'
     load: configs,
     envFilePath: ['.env.development', '.env.local'],
     validationSchema: schemas
-  })],
+  }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/users/interfaces/users.interface.ts
@@ -0,0 +1,9 @@
+import { registerEnumType } from "@nestjs/graphql";
+
+// 第三方可选值
+export enum ThirdPlatform {
+    Github = 'github'
+}
+registerEnumType(ThirdPlatform, {
+    name: 'ThirdPlatform'
+})
