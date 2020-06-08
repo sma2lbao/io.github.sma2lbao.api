f1ad40d0fc7498241e89ff069485807af929a5b7
sma2lbao <sma2lbao@qq.com>
2020-06-09 00:50:14 +0800

    mailer change inject

---
+++ src/core/mailer/mailer.constant.ts
@@ -0,0 +1,1 @@
+export const mailer_transporter = 'mailer_transporter';
--- src/core/mailer/mailer.service.ts
+++ src/core/mailer/mailer.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Transporter, createTransport, SendMailOptions } from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+import SMTPTransport, { SentMessageInfo } from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  constructor(private readonly transporter: Transporter) {}
+
+  async sendMail(options: SendMailOptions): Promise<SentMessageInfo> {
+    return await this.transporter.sendMail(options);
+  }
+}
