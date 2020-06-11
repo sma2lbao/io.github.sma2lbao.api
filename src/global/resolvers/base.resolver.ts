680f89ad1e00fb937f49ecfbf4d1d4217d5eba8a
李观保 <sma2lbao@qq.com>
2020-06-11 18:26:20 +0800

    add global base resolver.

---
+++ src/global/resolvers/base.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { Type } from '@nestjs/common';
+
+function BaseResolver<T extends Type<unknown>>(classRef: T): any {
+  @Resolver({ isAbstract: true })
+  abstract class BaseResolverHost {
+    @Query(type => [classRef], { name: `findAll${classRef.name}` })
+    async findAll(): Promise<T[]> {
+      return [];
+    }
+  }
+  return BaseResolverHost;
+}
