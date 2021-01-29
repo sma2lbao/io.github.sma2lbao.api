49521e610d8a311fb69e565bbfedbdd62b57ac05
李观保 <sma2lbao@qq.com>
2021-01-29 14:24:57 +0800

    secure change.

--- src/config/namespace/oss.config.ts
+++ src/config/namespace/oss.config.ts
@@ -3,9 +3,9 @@ import { registerAs } from '@nestjs/config';
 const { env } = process;
 
 export default registerAs('oss', () => ({
-  region: env.OSS_REGION || 'oss-cn-shenzhen',
-  accessKeyId: env.OSS_ACCESS_KEY_ID || 'LTAI4GHbBQaAuG8odUV34171',
+  region: env.OSS_REGION,
+  accessKeyId: env.OSS_ACCESS_KEY_ID,
   accessKeySecret:
-    env.OSS_ACCESS_KEY_SECRET || 'iH9on2ffnfKeHAWZHQX4N27o0asWyd',
-  bucket: env.OSS_BUCKET || 'miss-files',
+    env.OSS_ACCESS_KEY_SECRET,
+  bucket: env.OSS_BUCKET,
 }));
